7f03f1433d7cc268c3976a4a8a89ee8e3c5fe49f
b-kip <bruzkip@gmail.com>
2021-09-26 22:34:23 +0300

    Added outlay for better mobile layout functionality

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,20 +1,33 @@
+import { useState } from 'react';
 import hamburger from '../../assets/images/icon-hamburger.svg';
 import Logo from '../Logo';
 import NavLinks from './NavLinks';
 import { navLinks } from './navigationData';
 
 export default function Navigation() {
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
+
+  function toggleMobile() {
+    setShowMobileMenu(!showMobileMenu);
+  }
+
   return (
     <header className="header">
+      <div 
+        className={`mobile-overlay ${showMobileMenu ? '' : 'hidden'}`}
+        onClick={toggleMobile}
+      ></div>
       <div className="container">
         <nav className="navbar">
           <div className="navbar-top">
             <Logo />
-            <button className="hamburger" id="js-hamburger-button">
+            <button className="hamburger" onClick={toggleMobile}>
               <img src={hamburger} alt="harmburger icon" />
             </button>
           </div>
-          <div className="navbar-bottom bg-dark-violet hidden" id="js-navbar-bottom">
+          <div 
+            className={`navbar-bottom bg-dark-violet ${ showMobileMenu ? '' : 'hidden'}`}
+          >
             <NavLinks navLinks={navLinks} />
             <div className="navbar__buttons">
               <button className="btn btn--transparent">Login </button>
